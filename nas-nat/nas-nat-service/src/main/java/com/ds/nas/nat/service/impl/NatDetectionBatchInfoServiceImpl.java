5958c88039b24d6951d96d7571bf10e976fb11c1
ds <1159629515@qq.com>
2023-01-19 11:26:32 +0800

    ds

--- nas-nat/nas-nat-service/src/main/java/com/ds/nas/nat/service/impl/NatDetectionBatchInfoServiceImpl.java
+++ nas-nat/nas-nat-service/src/main/java/com/ds/nas/nat/service/impl/NatDetectionBatchInfoServiceImpl.java
@@ -34,7 +34,7 @@ public class NatDetectionBatchInfoServiceImpl extends ServiceImpl<NatDetectionBa
     @Resource
     private PersonalInfoClient personalInfoClient;
 
-    @DubboReference
+    @DubboReference(version = "1.0.0")
     private HealthCodeProvider healthCodeProvider;
 
     @Override
