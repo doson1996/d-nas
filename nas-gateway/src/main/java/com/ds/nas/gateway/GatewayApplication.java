b80e94c612cc7dcb4081115f14e9c7b4a81df9d8
ds <1159629515@qq.com>
2023-03-19 22:51:46 +0800

    ds

---
+++ nas-gateway/src/main/java/com/ds/nas/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.ds.nas.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author ds
+ * @date 2023/3/19
+ * @description 网关
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
