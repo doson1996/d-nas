46f2e78213ebacee4aa8840d9c4c7b32848cb8f4
ds <1159629515@qq.com>
2023-04-04 22:07:00 +0800

    feat: xxl-job fix:

--- nas-hc/nas-hc-dao/src/main/java/com/ds/nas/hc/dao/request/HealthCodeQueryRequest.java
+++ nas-hc/nas-hc-dao/src/main/java/com/ds/nas/hc/dao/request/HealthCodeQueryRequest.java
@@ -2,8 +2,8 @@ package com.ds.nas.hc.dao.request;
 
 import com.ds.nas.lib.common.base.annotation.Check;
 import com.ds.nas.lib.common.base.request.BaseRequest;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+//import io.swagger.annotations.ApiModel;
+//import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -12,14 +12,14 @@ import lombok.Data;
  * @description 查询入参
  */
 @Data
-@ApiModel(description = "查询入参")
+//@ApiModel(description = "查询入参")
 public class HealthCodeQueryRequest extends BaseRequest {
 
     /**
      * 身份证号码
      */
     @Check(require = true)
-    @ApiModelProperty(value = "身份证号码", required = true)
+  //  @ApiModelProperty(value = "身份证号码", required = true)
     private String idCard;
 
 }
--- nas-hc/nas-hc-dao/src/main/java/com/ds/nas/hc/dao/response/HealthCodeQueryResponse.java
+++ nas-hc/nas-hc-dao/src/main/java/com/ds/nas/hc/dao/response/HealthCodeQueryResponse.java
@@ -1,8 +1,8 @@
 package com.ds.nas.hc.dao.response;
 
 import com.ds.nas.lib.common.base.response.BaseResponse;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+//import io.swagger.annotations.ApiModel;
+//import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
@@ -13,43 +13,43 @@ import java.util.Date;
  * @description 查询返回
  */
 @Data
-@ApiModel(description = "查询返回")
+//@ApiModel(description = "查询返回")
 public class HealthCodeQueryResponse extends BaseResponse {
 
     /**
      * 姓名
      */
-    @ApiModelProperty(value = "姓名")
+    //@ApiModelProperty(value = "姓名")
     private String name;
 
     /**
      * 身份证号码
      */
-    @ApiModelProperty(value = "身份证号码")
+   // @ApiModelProperty(value = "身份证号码")
     private String idCard;
 
     /**
      * 住址
      */
-    @ApiModelProperty(value = "住址")
+   // @ApiModelProperty(value = "住址")
     private String address;
 
     /**
      * 手机号
      */
-    @ApiModelProperty(value = "手机号")
+    //@ApiModelProperty(value = "手机号")
     private String phone;
 
     /**
      * 0.未申领 1.绿码 2.黄码 3.红码 4.弹窗
      */
-    @ApiModelProperty(value = "0.未申领 1.绿码 2.黄码 3.红码 4.弹窗")
+    //@ApiModelProperty(value = "0.未申领 1.绿码 2.黄码 3.红码 4.弹窗")
     private Integer health;
 
     /**
      * 最后一次核酸时间
      */
-    @ApiModelProperty(value = "最后一次核酸时间")
+    //@ApiModelProperty(value = "最后一次核酸时间")
     private Date lastNucleicAcidTime;
 
 }
