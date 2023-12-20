4ca8e0d8875aa5ab680c83496de4b3bd0771c55b
ds <1159629515@qq.com>
2023-12-21 00:08:44 +0800

    ds

---
+++ nas-nat/nas-nat-app/src/main/java/com/ds/nas/nat/app/MySpringApplicationRunListener.java
@@ -0,0 +1,60 @@
+package com.ds.nas.nat.app;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * @author ds
+ * @date 2023/12/20
+ * @description
+ */
+@Slf4j
+public class MySpringApplicationRunListener implements SpringApplicationRunListener {
+
+    private final SpringApplication application;
+
+    private final String[] args;
+
+    public MySpringApplicationRunListener(SpringApplication application, String[] args) {
+        this.application = application;
+        this.args = args;
+    }
+
+    @Override
+    public void starting() {
+        log.info("starting...");
+    }
+
+    @Override
+    public void environmentPrepared(ConfigurableEnvironment environment) {
+        log.info("environmentPrepared...");
+    }
+
+    @Override
+    public void contextPrepared(ConfigurableApplicationContext context) {
+        log.info("contextPrepared...");
+    }
+
+    @Override
+    public void contextLoaded(ConfigurableApplicationContext context) {
+        log.info("contextLoaded...");
+    }
+
+    @Override
+    public void started(ConfigurableApplicationContext context) {
+        log.info("started...");
+    }
+
+    @Override
+    public void running(ConfigurableApplicationContext context) {
+        log.info("running...");
+    }
+
+    @Override
+    public void failed(ConfigurableApplicationContext context, Throwable exception) {
+        log.info("failed...");
+    }
+}
