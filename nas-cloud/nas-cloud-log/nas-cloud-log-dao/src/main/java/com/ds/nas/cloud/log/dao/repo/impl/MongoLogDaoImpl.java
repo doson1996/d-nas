11b20856d84161205ac28869ff00029ce3c010f8
ds <1159629515@qq.com>
2024-04-07 23:55:07 +0800

    mongo

--- nas-cloud/nas-cloud-log/nas-cloud-log-dao/src/main/java/com/ds/nas/cloud/log/dao/repo/impl/MongoLogDaoImpl.java
+++ nas-cloud/nas-cloud-log/nas-cloud-log-dao/src/main/java/com/ds/nas/cloud/log/dao/repo/impl/MongoLogDaoImpl.java
@@ -3,6 +3,8 @@ package com.ds.nas.cloud.log.dao.repo.impl;
 
 import com.ds.nas.cloud.log.dao.repo.LogDao;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
@@ -20,6 +22,9 @@ public class MongoLogDaoImpl implements LogDao {
 
     @Override
     public String insertOne(LogRequest request) {
+//        Query query = new Query(Criteria.where("age").gt("20"));
+//        mongoTemplate.find(query, Log.class);
+
         mongoTemplate.insert(request);
         return "ok";
     }
